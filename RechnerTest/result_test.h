483cbab2fa743bcbca75fb2747b4b953bf61db48
Phooba <33224079+JohannesMund@users.noreply.github.com>
2022-04-11 20:42:05 +0200

    Update result_test.h

--- RechnerTest/result_test.h
+++ RechnerTest/result_test.h
@@ -23,7 +23,6 @@ private slots:
         QTest::newRow("Test3") <<  -12345.6789 << "-12345.6789";
         QTest::newRow("Test4") <<  -12345.6789000000000000000001 << "-12345.6789";
         QTest::newRow("Test5") <<  12345.6789000000000000000001 << "12345.6789";
-
     }
 
     void test_addDigit()
@@ -37,10 +36,8 @@ private slots:
         QVERIFY(r.isValid());
         QCOMPARE(r.get(), in);
 
-
         QFETCH(QString, asString);
         QCOMPARE(r.toString(), asString);
-
     }
 };
 
