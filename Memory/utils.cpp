45df492d2276fda71cd7b9b8c469f42a641b3f81
Phooba <ich@shneeble.com>
2022-11-19 19:11:43 +0100

    Kommentare, kleinigkeiten

--- Memory/utils.cpp
+++ Memory/utils.cpp
@@ -5,10 +5,11 @@
 
 void Randomizer::shuffle(std::vector<unsigned int>& v)
 {
-    // Randomisierer initialisieren
+    /// Randomisierer initialisieren
+    /// Wir machen das jedes mal neu, damit wir auf jeden Fall eine neue Reihenfolge bekommen
     unsigned int seed = std::chrono::system_clock::now().time_since_epoch().count();
     auto rng = std::default_random_engine{seed};
 
-    // Zahlen schön mischeln
+    /// Zahlen schön mischeln
     std::shuffle(v.begin(), v.end(), rng);
 }
